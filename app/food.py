28e2bf2a82b16326979cadeff5d1f39033ab9adc
John Sastrillo <john.sastrillo@gmail.com>
2019-02-24 20:25:18 -0800

    added wall and food detection

--- app/food.py
+++ app/food.py
@@ -3,59 +3,118 @@ import json
 def calculateDirection(data):
     foods = data["board"]["food"]
     currPositions = data["you"]["body"]
+    height = data["board"]["height"]
+    width = data["board"]["width"]
 
-    #TODO: nearestFood = getNearestFood(foods)
+    nearestFood = getClosestFood(foods, currPositions[0])
 
-    return directionToFood(foods[0], currPositions)
+    return directionToFood(foods[0], currPositions, height, width)
 
-def directionToFood(food, bodyPositions):
+def directionToFood(food, bodyPositions, height, width):
     headPos = bodyPositions[0]
 
     dir = "left"
 
+    movedTried = []
+
     if int(food["y"]) < int(headPos["y"]):
-        if isPossibleMove("up", bodyPositions):
-            print("up")
+        if isPossibleMove("up", bodyPositions, height, width):
             return "up"
-    else:
-        if isPossibleMove("down", bodyPositions):
-            print("down")
+    elif int(food["y"]) > int(headPos["y"]):
+        if isPossibleMove("down", bodyPositions,height, width):
             return "down"
 
     if int(food["x"]) < int(headPos["x"]):
-        if isPossibleMove("left", bodyPositions):
-            print("left")
+        if isPossibleMove("left", bodyPositions, height, width):
             return "left"
-    else:
-        if isPossibleMove("right", bodyPositions):
-            print("right")
+    elif int(food["x"]) > int(headPos["x"]):
+        if isPossibleMove("right", bodyPositions, height, width):
+            return "right"
+
+    while len(movedTried) is not 4:
+        if "up" not in movedTried and isPossibleMove("up", bodyPositions, height, width):
+            movedTried.append("up")
+            return "up"
+        if "down" not in movedTried and isPossibleMove("down", bodyPositions, height, width):
+            movedTried.append("down")
+            return "down"
+        if "left" not in movedTried and isPossibleMove("left", bodyPositions, height, width):
+            movedTried.append("left")
+            return "left"
+        if "right" not in movedTried and isPossibleMove("right", bodyPositions, height, width):
+            movedTried.append("right")
             return "right"
 
     return dir
 
-def isPossibleMove(direction, bodyPositions):
+def isPossibleMove(direction, bodyPositions, height, width):
     headPos = bodyPositions[0]
 
     i = 1
-    #TODO: check for walls
     while i < len(bodyPositions):
 
+        headPos_x = headPos["x"]
+        headPos_y = headPos["y"]
+
+        bodyPos_x = bodyPositions[i]["x"]
+        bodyPos_y = bodyPositions[i]["y"]
+
         if direction == "up":
-            if headPos["y"] - bodyPositions[i]["y"] == 1:
+            if (headPos_y - bodyPos_y == 1 and headPos_x == bodyPos_x) or headPos_y == 0:
                 return False
         if direction == "down":
-            if bodyPositions[i]["y"] - headPos["y"] == 1:
+            if (bodyPos_y - headPos_y == 1 and headPos_x == bodyPos_x) or headPos_y == height - 1:
                 return False
         if direction == "right":
-            if bodyPositions[i]["x"] - headPos["x"] == 1:
+            if (bodyPos_x - headPos_x == 1 and headPos_y == bodyPos_y) or headPos_x == width - 1:
                 return False
         if direction == "left":
-            if headPos["x"] - bodyPositions[i]["x"] == 1:
+            if (headPos_x - bodyPos_x == 1 and headPos_y == bodyPos_y) or headPos["x"] == 0:
                 return False
 
         i = i + 1
 
     return True
+
+def getClosestFood(foods, headPos):
+
+    closestDistance = calculateDistance(foods[0], headPos)
+    closestFood = foods[0]
+
+    for food in foods:
+        newDistance = calculateDistance(food, headPos)
+        if closestDistance > newDistance:
+            closestDistance = newDistance
+            closestFood = food
+
+    print(closestFood)
+    print(closestDistance)
+    print(headPos)
+    return closestFood
+
+def calculateDistance(food, headPos):
+
+    distance = 0;
+
+    food_x = food["x"];
+    food_y = food["y"];
+
+    headPos_x = headPos["x"];
+    headPos_y = headPos["y"];
+
+    if headPos_x < food_x:
+        distance = distance + food_x - headPos_x
+    else:
+        distance = distance + headPos_x - food_x
+
+    if headPos_y < food_y:
+        distance = distance + food_y - headPos_y
+    else:
+        distance = distance + headPos_y - food_y
+
+    return distance
+
+
 
 
 
