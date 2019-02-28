57ae8a5789546253f1df6ba144c076966bcf876e
rbarket <barketrashid@hotmail.com>
2019-02-28 13:25:06 -0800

    made a dumb change and accidentally commented out important function

--- app/choose_dir.py
+++ app/choose_dir.py
@@ -18,19 +18,19 @@ def calculate_direction(data):
 
     print("turn number {}".format(data["turn"]))
 
-    # # Dying so go get food
-    # if foods and (health < 50 or data['turn'] < 4):
-    nearest_food = food.get_closest_food(foods, you[0])
-    print("headloc({} {}) + foodloc({} {})".format(body_pos[0]['x'], body_pos[0]['y'], nearest_food['x'], nearest_food['y']))
-    return food.direction_to_food(food=nearest_food,
-                                  you=you,
-                                  occupied=body_pos,
-                                  height=height,
-                                  width=width)
+    # Dying so go get food
+    if foods and (health < 50 or data['turn'] < 4):
+        nearest_food = food.get_closest_food(foods, you[0])
+        print("headloc({} {}) + foodloc({} {})".format(body_pos[0]['x'], body_pos[0]['y'], nearest_food['x'], nearest_food['y']))
+        return food.direction_to_food(food=nearest_food,
+                                      you=you,
+                                      occupied=body_pos,
+                                      height=height,
+                                      width=width)
 
-    # # Chase tail to stall out
-    # else:
-    #     return survive.find_tail(you=you,
-    #                              occupied=body_pos,
-    #                              width=width,
-    #                              height=height)
+    # Chase tail to stall out
+    else:
+        return survive.find_tail(you=you,
+                                 occupied=body_pos,
+                                 width=width,
+                                 height=height)
